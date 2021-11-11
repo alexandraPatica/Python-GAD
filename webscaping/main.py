c3a529232d1aca23de7e5bcbfdeca2a03eae67d1
alexandraPatica <alexandrapatica@yahoo.com>
2021-11-11 02:38:35 +0200

    .

--- webscaping/main.py
+++ webscaping/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+standings = []
+lpf_domain = 'http://frsah.ro/'
+columns = ['image', 'title', 'date', 'text']
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    page = requests.get(f'{lpf_domain}')
+    soup = BeautifulSoup(page.content, features='html.parser')
+
+    # content = soup.find('div', {"class": 'td-ss-main-content td_block_template_1'})
+    content = soup.find(class_='td-ss-main-content td_block_template_1').find_all('div')
+    print(content)
+
+    news = content.find_all('div', class_='td_module_16 td_module_wrap td-animation-stack')
+    # print(news)
+    for new in news:
+        divs = new.find_all('div')
+        text_from_divs = [
+            div for div in new.find_all('div')
+        ]
+        # print(text_from_divs)
+        data_from_news = []
+        img_src = divs[1].find('img')['src']
+        title = divs[2].find('h3')
+
+        data_from_news.append(divs[1].find())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
